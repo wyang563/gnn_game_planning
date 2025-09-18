e07aeaa4395f2a0809c7e23362a715f662087026
wyang563 <willyanglex@gmail.com>
2025-09-18 12:14:41 -0400

    set up w weights for runtime cost function

--- src/point_agent_lqr_solver.py
+++ src/point_agent_lqr_solver.py
@@ -15,6 +15,7 @@ class Agent(iLQR):
         u_dim: int, 
         Q: jnp.ndarray, 
         R: jnp.ndarray,
+        W: jnp.ndarray,
         horizon: int,
         x0: jnp.ndarray,
         u_traj: jnp.ndarray,
@@ -26,6 +27,7 @@ class Agent(iLQR):
         # simulation parameters 
         self.id = id
         self.horizon = horizon
+        self.w1, self.w2, self.w3, self.w4 = W[0], W[1], W[2], W[3] 
         self.x0 = x0
         self.u_traj = u_traj
         self.goal = goal
@@ -57,11 +59,11 @@ class Agent(iLQR):
     def runtime_loss(self, xt, ut, ref_xt, other_xts):
         collision_loss = 0.0
         for other_xt in other_xts:
-            collision_loss += 10.0 * jnp.exp(-5.0 * jnp.sum(jnp.square(xt[:2] - other_xt[:2])))
+            collision_loss += self.w1 * jnp.exp(-self.w2 * jnp.sum(jnp.square(xt[:2] - other_xt[:2])))
         # normalize collision loss 
         collision_loss /= other_xts.shape[0]
-        ctrl_loss: jnp.ndarray = 0.1 * jnp.sum(jnp.square(ut))
-        nav_loss: jnp.ndarray = jnp.sum(jnp.square(xt[:2]-ref_xt[:2]))
+        ctrl_loss: jnp.ndarray = self.w3 * jnp.sum(jnp.square(ut))
+        nav_loss: jnp.ndarray = self.w4 * jnp.sum(jnp.square(xt[:2]-ref_xt[:2]))
         return nav_loss + collision_loss + ctrl_loss
 
     def loss(self, x_traj, u_traj, ref_x_traj, other_x_trajs):
@@ -100,6 +102,7 @@ class Simulator:
         n_agents: int,
         Q: jnp.ndarray,
         R: jnp.ndarray, 
+        W: jnp.ndarray,
         horizon: int, 
         dt: float, 
         init_arena_range: Tuple[float, float], 
@@ -114,6 +117,7 @@ class Simulator:
         self.n_agents = n_agents
         self.Q = Q
         self.R = R
+        self.W = W
         self.horizon = horizon
         self.dt = dt
         self.timestep = 0
@@ -143,6 +147,7 @@ class Simulator:
                 u_dim=2,
                 Q=self.Q,
                 R=self.R,
+                W=self.W,
                 horizon=self.horizon,
                 x0=x0,
                 u_traj=u_traj,
@@ -202,23 +207,25 @@ if __name__ == "__main__":
     
     # Simulation parameters
     N = 10  # Number of agents
-    horizon = 300  # Optimization horizon (T steps)
+    horizon = 200  # Optimization horizon (T steps)
     dt = 0.05  # Time step
     init_arena_range = (-10.0, 10.0)  # Initial position range
     goal_threshold = 0.2  # Convergence threshold
     device = "cpu"
-    optimization_iters = 200  # Total simulation steps
+    optimization_iters = 500  # Total simulation steps
     step_size = 0.002
 
     # Cost weights (position, velocity, control)
     Q = jnp.diag(jnp.array([0.1, 0.1, 0.001, 0.001]))  # Higher position weights
     R = jnp.diag(jnp.array([0.01, 0.01]))
+    W = jnp.array([20.0, 5.0, 0.1, 1.0]) # w1 = collision, w2 = collision cost exp decay, w3 = control, w4 = navigation
 
     # Create simulator    
     simulator = Simulator(
         n_agents=N,
         Q=Q,
         R=R,
+        W=W,
         horizon=horizon,
         dt=dt,
         init_arena_range=init_arena_range,
